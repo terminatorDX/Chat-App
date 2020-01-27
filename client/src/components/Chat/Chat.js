323960ea88d7645c735b3d3f161934f3bd67961d
Chetan Jain <jain.cj.chetan@gmail.com>
2020-01-28 05:12:55 +0530

    negligible change

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
-import io from "socket.io-client";
 import "./Chat.css";
+import io from "socket.io-client";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 let socket;
